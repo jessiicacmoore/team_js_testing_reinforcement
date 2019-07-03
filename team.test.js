b5a3b1687e8afaffbfeedb82f8d42eca0b6d1aeb
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-03 10:27:20 -0400

    Completed writing basic test

--- team.test.js
+++ team.test.js
@@ -1,5 +1,9 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('Returns an object', () => {
+  const teamName = 'Unicron';
+  const level = 100;
+  const result = teamObj(teamName, level);
+
+  expect(result).toEqual({'teamName':teamName, 'level':level, 'points':0});
+});
