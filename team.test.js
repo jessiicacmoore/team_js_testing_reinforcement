d65f8209f4e346b16633852d45eb57bcc6f4301d
Jessica Moore <jessica.cmoore1@gmail.com>
2019-07-03 13:00:44 -0400

    Wrote some additional tests

--- team.test.js
+++ team.test.js
@@ -1,9 +1,24 @@
 const teamObj = require('./team.js');
 
-test('Returns an object', () => {
+test('Returns an object when function is called with args', () => {
   const teamName = 'Unicron';
   const level = 100;
   const result = teamObj(teamName, level);
-
-  expect(result).toEqual({'teamName':teamName, 'level':level, 'points':0});
+  const expectation = {'teamName':teamName, 'level':level, 'points':0};
+  expect(result).toEqual(expectation);
 });
+
+test('Returns Obj with undefined values when no args are provided', () => {
+  const result = teamObj();
+  const expectation = {'teamName':undefined, 'level':undefined, 'points':0};
+  expect(result).toEqual(expectation);
+});
+
+test('Points remain 0 even when trying to pass in a third arg', () => {
+  const teamName = 'Unicron';
+  const level = 100;
+  const result = teamObj(teamName, level, 50);
+  const expectation = {'teamName':teamName, 'level':level, 'points':0};
+  expect(result).toEqual(expectation);
+});
+
